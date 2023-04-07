f77b6844be24ab7e084b69e8d316eaae148082d7
jalebi <jalebi@sabacell.net>
2023-04-08 01:47:06 +0330

    fix: add taget

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  target: 'static',
   ssr: true,
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
