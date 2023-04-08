faa300be6f7d2ab6a7a19f091d6531fb56656b86
jalebi <mehrdad.jalebi@gmail.com>
2023-04-08 10:44:09 +0330

    fix: remove dist

--- .dist/composition-api/globals.mjs
+++
@@ -1,5 +0,0 @@
-export const isFullStatic = true
-export const staticPath = "/home/mehrdad/countriesList/.dist/static-json"
-export const publicPath = "/"
-export const globalContext = "__NUXT__"
-export const globalNuxt = "$nuxt"
